69bc2a6d0fbc0c7952f1efcb717c5598da8072cb
scarlettcanny0629 <83673835+scarlettcanny0629@users.noreply.github.com>
2021-08-03 21:55:09 -0400

    Add files via upload

--- create_pilot_odms.R
+++ create_pilot_odms.R
@@ -15,9 +15,9 @@ library(ondisc)
 # gene count matrix
 gene_odm_to_save_fp <- paste0(processed_data_dir, "gene_expression_odm.rds")
 if (!file.exists(gene_odm_to_save_fp)) {
-  gRNA_matrix <- readMM(paste0(raw_data_dir, "GSE120861_pilot_highmoi_screen.exprs.mtx"))
-  barcodes <- read_tsv(paste0(raw_data_dir, "GSE120861_pilot_highmoi_screen.cells.txt"),col_names = FALSE)
-  gene_ids <- read_tsv(paste0(raw_data_dir, "GSE120861_pilot_highmoi_screen.genes.txt"),col_names = FALSE)
+  gRNA_matrix <- Matrix::readMM(paste0(raw_data_dir, "GSE120861_pilot_highmoi_screen.exprs.mtx"))
+  barcodes <- readr::read_tsv(paste0(raw_data_dir, "GSE120861_pilot_highmoi_screen.cells.txt"),col_names = FALSE)
+  gene_ids <- readr::read_tsv(paste0(raw_data_dir, "GSE120861_pilot_highmoi_screen.genes.txt"),col_names = FALSE)
   gene_expression_metadata_odm <- create_ondisc_matrix_from_R_matrix(r_matrix = gRNA_matrix,
                                                      barcodes = as.character(dplyr::pull(barcodes)),
                                                      features_df = as.data.frame(as.character(dplyr::pull(gene_ids)),stringsAsFactors=FALSE),
