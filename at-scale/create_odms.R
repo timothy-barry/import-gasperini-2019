98128a6e61aad388b29ca28daaf0d4c80badb860
Tim <tbarry2@andrew.cmu.edu>
2021-08-11 18:38:28 -0400

    separate grouped and ungrouped gRNA datasets

--- at-scale/create_odms.R
+++ at-scale/create_odms.R
@@ -1,22 +1,39 @@
+###########################
+# 0. Load packages; set fps
+###########################
 # source config file to get gasperini offsite location
 gasp_offsite <- paste0(.get_config_path("LOCAL_GASPERINI_2019_DATA_DIR"), "at-scale/")
 
-# create the processed data directory; set the raw and intermediate directories
+# create the processed data directories
 processed_data_dir <- paste0(gasp_offsite, "processed/")
-if (!dir.exists(processed_data_dir)) dir.create(path = processed_data_dir, recursive = TRUE)
+processed_data_dir_gene <- paste0(processed_data_dir, "gene/")
+processed_data_dir_grouped <- paste0(processed_data_dir, "gRNA_grouped/")
+processed_data_dir_ungrouped <- paste0(processed_data_dir, "gRNA_ungrouped/")
+dirs_to_create <- c(processed_data_dir_gene,
+                    processed_data_dir_grouped,
+                    processed_data_dir_ungrouped)
+for (dir in dirs_to_create) {
+  if (!dir.exists(dir)) dir.create(path = dir, recursive = TRUE)
+} 
+
+# set raw directories
 raw_data_dir <- paste0(gasp_offsite, "raw/")
 intermediate_data_dir <- paste0(gasp_offsite, "intermediate/")
 
-# load ondisc
+# load packages
+library(magrittr)
 library(ondisc)
 
+###########################
+# 1. gene expression matrix
+###########################
 # gene count matrix
 mtx_fp <- paste0(raw_data_dir, "GSE120861_at_scale_screen.exprs.mtx")
 barcodes_fp <- paste0(raw_data_dir, "GSE120861_at_scale_screen.cells.txt")
 gene_ids_fp <- paste0(raw_data_dir, "GSE120861_at_scale_screen.genes.txt")
 
-odm_fp <- paste0(processed_data_dir, "gasp_scale_gene_expressions.odm")
-metadata_fp <- paste0(processed_data_dir, "gasp_scale_gene_metadata.rds")
+odm_fp <- paste0(processed_data_dir_gene, "gasp_scale_gene_expressions.odm")
+metadata_fp <- paste0(processed_data_dir_gene, "gasp_scale_gene_metadata.rds")
 
 # create the odm 
 gene_odm <- create_ondisc_matrix_from_mtx(mtx_fp = mtx_fp, barcodes_fp = barcodes_fp, 
@@ -32,15 +49,65 @@ gene_odm_plus_pmito_batch <- mutate_cell_covariates(gene_odm, p_mito = gasp_cell
 save_odm(odm = gene_odm_plus_pmito_batch,
          metadata_fp = metadata_fp)
 
-
+##################################
+# 2. gRNA count matrix (ungrouped)
+##################################
 # next, load the gRNA count matrix. Write the backing .odm file and metadata file to disk.
-odm_fp <- paste0(processed_data_dir, "gasp_scale_gRNA_counts.odm")
-metadata_fp <- paste0(processed_data_dir, "gasp_scale_gRNA_metadata.rds")
+odm_fp <- paste0(processed_data_dir_ungrouped, "gasp_scale_gRNA_counts_ungrouped.odm")
+metadata_fp <- paste0(processed_data_dir_ungrouped, "gasp_scale_gRNA_metadata_ungrouped.rds")
 gRNA_count_matrix <- readRDS(paste0(intermediate_data_dir, "gRNA_count_matrix.rds"))
+gRNA_groups_df <- readr::read_tsv(file = paste0(raw_data_dir,
+                                             "GSE120861_grna_groups.at_scale.txt"),
+                               col_types = "cc", col_names = c("gRNA_group", "barcodes"))
+# confirm that ordering of gRNA groups matches that of barcodes
+identical(row.names(gRNA_count_matrix), gRNA_groups_df$barcodes)
 cell_barcodes <- colnames(gRNA_count_matrix)
-feature_barcodes <- data.frame(row.names(gRNA_count_matrix))
+# create the gRNA odm
 gRNA_odm <- create_ondisc_matrix_from_R_matrix(r_matrix = gRNA_count_matrix,
                                                barcodes = cell_barcodes,
-                                               features_df = feature_barcodes,
+                                               features_df = gRNA_groups_df[,c(2,1)],
+                                               odm_fp = odm_fp,
+                                               metadata_fp = metadata_fp)
+
+################################
+# 3. gRNA count matrix (grouped)
+################################
+tab <- table(gRNA_groups_df$gRNA_group)
+single_gRNA_groups <- names(tab)[which(tab == 1)]
+double_gRNA_groups <- names(tab)[which(tab == 2)]
+
+# extract the first and second sets of barcodes for the doubles
+doubles_grouped <- dplyr::filter(gRNA_groups_df, gRNA_group %in% double_gRNA_groups) %>%
+  dplyr::group_by(gRNA_group) %>% dplyr::arrange(gRNA_group) 
+doubles_barcode_1 <- doubles_grouped %>% dplyr::summarize(barcode = barcodes[1]) %>%
+  dplyr::pull(barcode)
+doubles_barcode_2 <- doubles_grouped %>% dplyr::summarize(barcode = barcodes[2]) %>%
+  dplyr::arrange(gRNA_group) %>% dplyr::pull(barcode)
+ordered_groups <- unique(doubles_grouped$gRNA_group)
+# verify on a few of the groups that the extraction was correct
+for (i in sample(seq(1, length(ordered_groups)), 15, FALSE)) {
+  grp <- ordered_groups[i]
+  bc_check <- c(doubles_barcode_1[i], doubles_barcode_2[i])
+  bc_test <- dplyr::filter(doubles_grouped, gRNA_group == grp) %>% dplyr::pull(barcodes)
+  testthat::expect_true((bc_check %in% bc_test) && (bc_test %in% bc_check))
+}
+# sum the count vectors
+double_m1 <- gRNA_count_matrix[doubles_barcode_1,]
+double_m2 <- gRNA_count_matrix[doubles_barcode_2,]
+double_sum <- double_m1 + double_m2
+row.names(double_sum) <- ordered_groups
+# append "double_sum" to the matrix of counts of singlet groups
+singlet_map <- dplyr::filter(gRNA_groups_df, gRNA_group %in% single_gRNA_groups)
+singlet_m <- gRNA_count_matrix[singlet_map$barcodes,]
+row.names(singlet_m) <- singlet_map$gRNA_group
+# create the final, "combined" matrix
+combined_matrix <- rbind(double_sum, singlet_m)
+combined_matrix <- as(combined_matrix, "dgTMatrix")
+# set the file paths
+odm_fp <- paste0(processed_data_dir_grouped, "gasp_scale_gRNA_counts_grouped.odm")
+metadata_fp <- paste0(processed_data_dir_grouped, "gasp_scale_gRNA_metadata_grouped.rds")
+gRNA_odm <- create_ondisc_matrix_from_R_matrix(r_matrix = combined_matrix,
+                                               barcodes = colnames(combined_matrix),
+                                               features_df = data.frame(gRNA_id = row.names(combined_matrix)),
                                                odm_fp = odm_fp,
                                                metadata_fp = metadata_fp)
--- at-scale/obtain_gRNA_gene_pairs.R
+++ at-scale/obtain_gRNA_gene_pairs.R
@@ -1,31 +1,42 @@
+#######################
+# 0. Load data; set fps
+#######################
 require(magrittr)
-# source config file to get gasperini offsite location
+
 gasp_offsite <- paste0(.get_config_path("LOCAL_GASPERINI_2019_DATA_DIR"), "at-scale/")
 raw_dir <- paste0(gasp_offsite, "raw/")
 processed_dir <- paste0(gasp_offsite, "processed/")
+processed_dir_grouped <- paste0(processed_dir, "gRNA_grouped/")
+processed_dir_ungrouped <- paste0(processed_dir, "gRNA_ungrouped/")
 
 # read gRNA-gene pairs and gRNA info
-all_results <- readr::read_tsv(file = paste0(raw_dir, "GSE120861_all_deg_results.at_scale.txt"), col_names = TRUE, col_types = "c")
-gRNA_groups <- readr::read_tsv(file = paste0(raw_dir, "GSE120861_grna_groups.at_scale.txt"), col_names = FALSE, col_types = "c") %>% dplyr::rename("gRNA_group" = "X1", "barcode" = "X2")
-
-# obtain the pairs to analyze from the results
-pairs_to_analyze <- all_results %>% dplyr::select(gene_id = ENSG, gRNA_group, gRNA_quality = quality_rank_grna, site_type)
-
-# We see that the gene-gRNA pairs of "pairs_to_analyze" are unique. However, the gRNA groups are duplicated in "gRNA_groups."
-pairs_to_analyze %>% dplyr::summarize(paste0(gene_id, "-", gRNA_group)) %>% dplyr::pull() %>% duplicated() %>% any()
-gRNA_groups$gRNA_group %>% duplicated() %>% any()
-
-# merge the gRNA_groups with all_results
-pairs_to_analyze_expanded <- dplyr::left_join(x = pairs_to_analyze, y = gRNA_groups, by = "gRNA_group") %>%
-  dplyr::rename("gRNA_id" = "barcode") %>% dplyr::mutate_all(factor)
-
-# save pairs to analyze
-saveRDS(object = pairs_to_analyze_expanded, file = paste0(processed_dir, "gRNA_gene_pairs.rds"))
-
-# save sampled pairs as well for testing purposes
+all_results <- readr::read_tsv(file = paste0(raw_dir, "GSE120861_all_deg_results.at_scale.txt"),
+                               col_names = TRUE, col_types = "c")
+gRNA_groups <- readr::read_tsv(file = paste0(raw_dir, "GSE120861_grna_groups.at_scale.txt"),
+                               col_names = FALSE, col_types = "c") %>% dplyr::rename("gRNA_group" = "X1", "barcode" = "X2")
 set.seed(4)
-pairs_sample <- pairs_to_analyze_expanded %>% dplyr::filter(gene_id %in% c("ENSG00000077514", "ENSG00000077157",
-                                                                           "ENSG00000076662", "ENSG00000076554") &
-                                                            gRNA_group %in% paste0("random_", seq(1L, 3))) %>%
-  dplyr::slice_sample(n = 15)
-saveRDS(object = pairs_sample, file = paste0(processed_dir, "gRNA_gene_pairs_sample.rds"))
+sample_pairs <- function(df) {
+  df %>% dplyr::filter(gene_id %in% c("ENSG00000077514", "ENSG00000077157",
+                               "ENSG00000076662", "ENSG00000076554") &
+                         gRNA_id %in% paste0("random_", seq(1L, 3))) %>% dplyr::slice_sample(n = 15)
+}
+
+#######################
+# 1. Save grouped pairs
+#######################
+grouped_pairs <- all_results %>% 
+  dplyr::select(gene_id = ENSG, gRNA_id = gRNA_group, gRNA_quality = quality_rank_grna, site_type) %>%
+  dplyr::mutate_all(factor)
+grouped_pairs_sample <- sample_pairs(grouped_pairs)
+
+saveRDS(grouped_pairs, paste0(processed_dir_grouped, "pairs_grouped.rds"))
+saveRDS(grouped_pairs_sample, paste0(processed_dir_grouped, "pairs_grouped_sample.rds"))
+
+#########################
+# 2. Save ungrouped pairs
+#########################
+pairs_to_analyze <- all_results %>%
+  dplyr::select(gene_id = ENSG, gRNA_group, gRNA_quality = quality_rank_grna, site_type)
+pairs_to_analyze_expanded <- dplyr::left_join(x = pairs_to_analyze, y = gRNA_groups, by = "gRNA_group") %>% 
+  dplyr::mutate_all(factor)
+saveRDS(pairs_to_analyze_expanded, paste0(processed_dir_ungrouped, "pairs_ungrouped.rds"))
